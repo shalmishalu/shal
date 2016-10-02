c53809589652d2277074f3565f0c880d1a34d213
shalmishalu <shalushalmi@gmail.com>
2016-10-02 14:36:02 +0530

    Add files via upload

---
+++ Categorytest1.java
@@ -0,0 +1,14 @@
+package niit.foodgro.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Categorytest1 {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
